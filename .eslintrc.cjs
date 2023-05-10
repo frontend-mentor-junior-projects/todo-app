7f0bcdb68a1a00138c9da6676b40da09befd84a7
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-05-10 14:39:59 +0200

    chore: setup eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,106 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier', // should always be the last one.
+  ],
+  overrides: [
+    {
+      files: ['**/*.stories.@(js|jsx|ts|tsx)'],
+      rules: {
+        'react/jsx-props-no-spreading': ['off'],
+      },
+    },
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    // Disable React import.
+    'react/react-in-jsx-scope': 0,
+    // Sort imports.
+    'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    // Require default arguments for optional props.
+    'react/require-default-props': [
+      'error',
+      {
+        forbidDefaultForRequired: true,
+        // functions: 'defaultArguments',
+      },
+    ],
+    // Allow use of arrow functions.
+    'arrow-body-style': ['warn', 'always'],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: ['function-declaration', 'arrow-function'],
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    // Disable extraneous dependencies for tests and specific files.
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          // Repos with a single test file.
+          'test.{ts,tsx}',
+          // Repos with multiple top-level test files.
+          'test-*.{ts,tsx}',
+          // Tests where the extension or filename suffix denotes that it is a test.
+          '**/*{.,_}{test,spec,stories}.{ts,tsx}',
+          // Jest config file.
+          '**/jest.config.ts',
+          // Jest setup file.
+          '**/jest.setup.ts',
+          // Vitest setup file.
+          '**/vitest.setup.ts',
+          // Cypress config file.
+          '**/cypress.config.ts',
+          // Vite config file.
+          'vite.config.ts',
+        ],
+        optionalDependencies: false,
+      },
+    ],
   },
 }
